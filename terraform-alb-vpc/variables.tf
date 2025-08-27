6958f107dc7487e9749fdf4fd574b08c6b06b1c0
srimuvva24 <srimuvva24@gmail.com>
2025-08-26 22:12:12 -0500

    Create variables.tf

---
+++ terraform-alb-vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "availability_zones" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b"]
+}
