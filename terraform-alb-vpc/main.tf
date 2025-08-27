cd7ad4e4abc24609a2061f059514383f44dd62a5
srimuvva24 <srimuvva24@gmail.com>
2025-08-27 14:09:23 -0500

    Update main.tf

--- terraform-alb-vpc/main.tf
+++ terraform-alb-vpc/main.tf
@@ -26,15 +26,6 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = var.availability_zones[0]
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
 
 # -----------------
 # Internet Gateway
@@ -70,44 +61,6 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# -----------------
-# NAT Gateway
-# -----------------
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public[0].id
-  depends_on    = [aws_internet_gateway.igw]
-
-  tags = {
-    Name = "nat-gateway"
-  }
-}
-
-# -----------------
-# Private Route Table
-# -----------------
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "private-rt"
-  }
-}
-
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private_rt.id
-}
 
 # -----------------
 # Security Group for ALB
