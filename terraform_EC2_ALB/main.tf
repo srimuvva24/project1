00417fce8ce18445ca8648b7436a438c6f32698d
srimuvva24 <your_email@example.com>
2025-08-27 13:16:43 -0500

    updated

---
+++ terraform_EC2_ALB/main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = "us-east-2"   # Change to your region
+}
+
+# Security Group allowing HTTP (8000) and SSH
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP 8000"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  count         = 2
+  ami           = "ami-0863fb82a7836e852"  # Amazon Linux 2 AMI (update for your region)
+  instance_type = "t2.micro"
+  key_name      = "mytest"   # Using your existing key
+  security_groups = [aws_security_group.web_sg.name]
+  iam_instance_profile = aws_iam_instance_profile.existing_role_profile.name
+  # Pick AZ based on count index
+  availability_zone = element(["us-east-2a", "us-east-2b"], count.index)
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo -u ec2-user bash -c "cd /home/ec2-user/tech-challenge-flask-app-main && source venv/bin/activate && export TC_DYNAMO_TABLE=Candidates && nohup gunicorn -b 0.0.0.0:8000 app:candidates_app &"
+  EOF
+
+  tags = {
+    Name = "flask-web-app"
+  }
+}
+
+resource "aws_iam_instance_profile" "existing_role_profile" {
+  name = "ec2-instance-profile"
+  role = "mypocrole"  # Replace with your IAM role name
+}
+
