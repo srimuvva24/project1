cce0ffa3526b5ecc7a55ae5ba8362f68fc9d2488
srimuvva24 <srimuvva24@gmail.com>
2025-08-26 21:36:25 -0500

    Update main.tf

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -10,13 +10,3 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "tf-sanity-test-bucket-${random_id.bucket_suffix.hex}"
   acl    = "private"
 }
-
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-2"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.test_bucket.id
-}
