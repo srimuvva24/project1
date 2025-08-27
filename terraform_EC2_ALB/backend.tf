ae94ca73f45aaf6d3fd3e0b2a0751981de86e36b
srimuvva24 <srimuvva24@gmail.com>
2025-08-27 13:18:53 -0500

    Update backend.tf

--- terraform_EC2_ALB/backend.tf
+++ terraform_EC2_ALB/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket0827"   # Replace with your S3 bucket name
-    key            = "ec2/terraform.tfstate"   # Path inside the bucket
+    key            = "ec2alb/terraform.tfstate"   # Path inside the bucket
     region         = "us-east-2"                   # S3 bucket region
     encrypt        = true                          # Encrypt state at rest with SSE-S3
   }
