38b2a4e3ab3811c3456d1c366119d85168e4ee15
srimuvva24 <srimuvva24@gmail.com>
2025-08-28 15:07:57 -0500

    Update main.tf

--- terraform_AMI/main.tf
+++ terraform_AMI/main.tf
@@ -1,6 +1,6 @@
 # Create AMI from existing instance
 resource "aws_ami_from_instance" "example" {
-  name               = "my-ami-from-${var.ec2_instance_id}-${timestamp()}"
+  name               = "my-ami-${var.ec2_instance_id}-${formatdate("YYYYMMDD", timestamp())}"
   source_instance_id = var.ec2_instance_id
   description        = "AMI created from EC2 instance ${var.ec2_instance_id}"
   
