82a1587e34cc49f011533eaebc31569b001a21f7
srimuvva24 <srimuvva24@gmail.com>
2025-08-28 14:50:54 -0500

    Update main.tf

--- terraform_AMI/main.tf
+++ terraform_AMI/main.tf
@@ -4,15 +4,9 @@ resource "aws_ami" "from_existing_instance" {
   source_instance_id = var.ec2_instance_id
   description        = "AMI created from EC2 instance ${var.ec2_instance_id}"
   # Optional: reboot the instance before creating AMI
-  reboot = true
+  reboot = false
 
   tags = {
     Name = "AMI-from-${var.ec2_instance_id}"
   }
-}
-
-# Output the AMI ID
-output "ami_id" {
-  value       = aws_ami.from_existing_instance.id
-  description = "The ID of the created AMI"
 }
