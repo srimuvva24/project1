6fc317d4247fd3b7ee28aba561ea5ba9a73c982f
srimuvva24 <srimuvva24@gmail.com>
2025-08-28 14:45:54 -0500

    Update variables.tf

--- terraform_AMI/variables.tf
+++ terraform_AMI/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
+
+# Input variable for the existing EC2 instance ID
+variable "ec2_instance_id" {
+  description = "The ID of the existing EC2 instance to create an AMI from"
+  type        = string
+}
