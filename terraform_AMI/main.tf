a8f0547ccefaef32d8e8198e49c7545716789410
srimuvva24 <srimuvva24@gmail.com>
2025-08-28 15:01:51 -0500

    Update main.tf

--- terraform_AMI/main.tf
+++ terraform_AMI/main.tf
@@ -3,8 +3,7 @@ resource "aws_ami_from_instance" "example" {
   name               = "my-ami-from-${var.ec2_instance_id}-${timestamp()}"
   source_instance_id = var.ec2_instance_id
   description        = "AMI created from EC2 instance ${var.ec2_instance_id}"
-  no_reboot          = false  # set true if you want to avoid reboot
-
+  
   tags = {
     Name = "AMI-from-${var.ec2_instance_id}"
   }
