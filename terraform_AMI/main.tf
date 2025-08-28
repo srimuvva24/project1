3240211d212194930010fbb896c47e989e0a0c68
srimuvva24 <srimuvva24@gmail.com>
2025-08-28 14:56:12 -0500

    Update main.tf

--- terraform_AMI/main.tf
+++ terraform_AMI/main.tf
@@ -1,10 +1,9 @@
 # Create AMI from existing instance
-resource "aws_ami" "from_existing_instance" {
+resource "aws_ami_from_instance" "example" {
   name               = "my-ami-from-${var.ec2_instance_id}-${timestamp()}"
   source_instance_id = var.ec2_instance_id
   description        = "AMI created from EC2 instance ${var.ec2_instance_id}"
-  # Optional: reboot the instance before creating AMI
-  reboot = false
+  no_reboot          = false  # set true if you want to avoid reboot
 
   tags = {
     Name = "AMI-from-${var.ec2_instance_id}"
