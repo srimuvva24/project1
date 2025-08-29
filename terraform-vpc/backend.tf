f6da00f8d3895e6b5c59e2d6a65e6ad748d8201e
srimuvva24 <srimuvva24@gmail.com>
2025-08-29 09:50:31 -0500

    Update backend.tf

--- terraform-vpc/backend.tf
+++ terraform-vpc/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket0827"   # Replace with your S3 bucket name
     key            = "alb-vpc/terraform.tfstate"   # Path inside the bucket
-    region         = "us-east-2"                   # S3 bucket region
+    region         = var.aws_region                   # S3 bucket region
     encrypt        = true                          # Encrypt state at rest with SSE-S3
   }
 }
